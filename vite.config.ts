ae5106588d1ea7831b97f21ae7c445fd6ed25abb
Rahulwagh07 <rahulwagh3774@gmail.com>
2025-01-27 06:10:32 +0530

    update vite and vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+    exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
   },
-  build: {
-    rollupOptions: {
-      external: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
-    }
-  }
-})
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
+  },
+});
